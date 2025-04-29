e3d42f06c91862c6dec80fb40e5d858e0ec5976a
lengockha2510 <ngockhai25102k2@gmail.com>
2025-04-29 16:36:37 +0700

    Cập nhật nội dung mới

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   base: '/', // ⚠️ THÊM DÒNG NÀY để đảm bảo router hoạt động tốt khi deploy
-  plugins: [react()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
